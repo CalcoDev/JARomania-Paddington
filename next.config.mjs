8c92bef1f7dc94c39fd133d5d70b37098169a62d
Calco Dev <calcopoddev@gmail.com>
2024-04-15 23:54:06 +0300

    The previous fix did more wrong.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/jaromania-paddington",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/jaromania-paddington" : undefined,
+  images: {unoptimized: true},
+  experimental: {images: {unoptimized: true}},
+//   assetPrefix: process.env.NODE_ENV === "production" ? "/jaromania-paddington" : undefined,
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
