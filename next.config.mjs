1226fc2e9cab867ba5edfe20b2c27a0d7fc27cbc
Calco Dev <calcopoddev@gmail.com>
2024-04-15 23:55:54 +0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/jaromania-paddington",
-  images: {unoptimized: true},
-  experimental: {images: {unoptimized: true}},
-//   assetPrefix: process.env.NODE_ENV === "production" ? "/jaromania-paddington" : undefined,
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
 };
 
 export default nextConfig;
