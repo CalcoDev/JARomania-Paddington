7256e877a6ce049ebaed502aadd824e4df24e4fd
Calco Dev <calcopoddev@gmail.com>
2024-04-15 23:47:48 +0300

    Asset path fix.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/jaromania-paddington",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/jaromania-paddington" : undefined,
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
